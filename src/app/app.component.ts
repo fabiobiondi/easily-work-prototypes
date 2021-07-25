ee40329cf1c55c2e13b74ec8735fd1b9f1c4f100
Fabio Biondi <fbiondi77@gmail.com>
2021-07-25 22:24:14 +0200

    add tailwind

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,10 @@ import {Component, Input, ViewChild} from '@angular/core';
 @Component({
   selector: 'fb-root',
   template: `
+    <button
+      class="py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-red-400">Hello</button>
 
+    <button class="btn">clap</button>
     <router-outlet></router-outlet>
 
   `,
